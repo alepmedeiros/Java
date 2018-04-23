100a507f497513fac058e1c549805a51442d8420
Alessandro Porto <alemedeiros@bsd.com.br>
2018-04-23 13:16:33 -0300

    Adicionando o arquivo .gitignore

---
+++ cobranca/src/main/java/com/example/cobranca/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.example.cobranca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.cobranca.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long>{	
+	
+}
