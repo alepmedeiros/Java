6d7598ab70015e8922f4a76bbcb139d046207db6
Alessandro Porto <alemedeiros@bsd.com.br>
2018-05-20 17:25:40 -0300

    Projeto de estudo Spring MVC

--- cobranca/src/main/java/com/example/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/example/cobranca/controller/TituloController.java
@@ -4,36 +4,55 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.cobranca.model.StatusTitulo;
 import com.example.cobranca.model.Titulo;
 import com.example.cobranca.repository.Titulos;
+import com.example.cobranca.service.CadastroTituloService;
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
 
+	private static final String CADASTRO_VIEW = "CadastroTitulo"; 
+	
 	@Autowired
 	private Titulos titulos;
 	
+	@Autowired
+	private CadastroTituloService cadastroTituloService;
+	
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(new Titulo());
 		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(Titulo titulo) {
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attribute){
+		if(errors.hasErrors()) {
+			return CADASTRO_VIEW;
+		}
 		
-		titulos.save(titulo);
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		mv.addObject("mensagem", "Título salvo com sucesso!");
-		return mv;
+		try {
+			cadastroTituloService.salvar(titulo);		
+			attribute.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+			return "redirect:/titulos/novo";
+		} catch(DataIntegrityViolationException e) {
+			errors.rejectValue("dataVencimento", null, e.getMessage());
+			return CADASTRO_VIEW;
+		}
 	}
 	
 	@RequestMapping
@@ -44,9 +63,30 @@ public class TituloController {
 		return mv;
 	}
 	
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
+		return mv;
+	}
+	
+	@RequestMapping(value = "{codigo}",method = RequestMethod.DELETE)
+	public String excluir(@PathVariable long codigo, RedirectAttributes attribute) {
+		cadastroTituloService.excluir(codigo);
+		attribute.addFlashAttribute("mensagem", "Título excluído com sucesso!");
+		return "redirect:/titulos";
+	}
+	
+
+	@RequestMapping(value = "/{codigo}/receber", method = RequestMethod.PUT)
+	public String receber(@PathVariable Long codigo) {
+		System.out.println("codig >>> " + codigo);
+		return "ok";		
+	}
+	
 	@ModelAttribute("statusTitulos")
 	public List<StatusTitulo> todosStatusTitulos(){
 		return Arrays.asList(StatusTitulo.values());
 	}
-	
+
 }
--- cobranca/src/main/java/com/example/cobranca/model/Titulo.java
+++ cobranca/src/main/java/com/example/cobranca/model/Titulo.java
@@ -11,7 +11,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 
@@ -22,13 +26,17 @@ public class Titulo {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
-	
+	@NotEmpty(message = "Descrição obrigatória")
+	@Size(max = 60, message = "A descrição não pode conter mais de 60 catacteres")
 	private String descricao;
 	
+	@NotNull(message = "Data de vencimento é obrigatória")
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
 		
+	@NotNull(message = "Valor obrigatório")
+	@DecimalMin(value = "0.01", message = "Valor não pode ser menor que 0,01")
 	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valor;
 	
